6fc5923ae6030744be1146acbc65567de4e11597
shjeng <ghsmsl20@gmail.com>
2024-04-07 14:03:46 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,15 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 
 public class SingletonWithPrototypeTest1 {
     @Test
@@ -28,29 +32,30 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2); // 원래 1이어야 하지만 프로토 타입 빈이 싱글톤 빈 안에 있기 때문에 2가 반환이 됐음.
-
-        // singleton 내에 prototype빈을 주입 받으면 안됨.
-
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
+    @Component
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        // ObjectProvider : 항상 새로운 프로토 타입 빈이 생성됨.
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
     @Scope("prototype")
+    @Component
     static class PrototypeBean {
         private int count = 0;
 
