714ea22534b7a383307a087a0f3eb023eddc729c
shjeng <ghsmsl20@gmail.com>
2024-04-10 00:13:50 +0900

    빈 스코프 - 스코프와 프록시(Spring 강의 끝)

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,16 +13,15 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
 
     /*
-    * MyLogger는 request로 요청이 왔을 때 생성되어야 하는데 Controller는 서버가 띄워지자마자 생성되기 때문에 생명주기가 맞지 않아서
-    * 오류가 발생함.
-    * */
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+     * MyLogger는 request로 요청이 왔을 때 생성되어야 하는데 Controller는 서버가 띄워지자마자 생성되기 때문에 생명주기가 맞지 않아서
+     * 오류가 발생함.
+     * */
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
